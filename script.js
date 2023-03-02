c3a1805de28c54bd4d8f22b58c5ef01ec305bec8
junaid <onlyjunaid2002@gmail.com>
2023-03-02 08:33:00 +0530

    Fdvcx

--- script.js
+++ script.js
@@ -1,79 +1,50 @@
+function show(x) {
+  x.children[2].style.display = "block";
+  //  x.children[1].style.color = "white";
+}
+function hide(x) {
+  x.children[2].style.display = "none";
+  // x.children[1].style.color = "black";
+}
+function news(x) {
+  if (x === 'donation') {
 
-//  fetch("/data/alumnicarousel.json")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//         let alum = `<div class="swiper-slide w-max h-[460px] ">
-//         <div class="flex flex-col rounded shadow overflow-hidden">
-//           <div class="flex-shrink-0 relative alumni" onmouseover="show(this)" onmouseleave="hide(this)" onclick="show(this)">
-//             <img class="h-[50vh] w-full object-cover" src="https://images.squarespace-cdn.com/content/v1/5cdcc14ad86cc91b80cdbf65/1558486905146-N2NCK3RXK9ZJ1PLRMEDN/Raj+Headshot+2-2019.jpg?format=750w" alt="">
-// <div class="absolute bottom-0 p-6  z-50 alumni-content text-white">
-// <p class="text-3xl font-semibold">Raj soin</p>
-// <p >Class 1987</p>
-// </div>
-//             <div class="absolute z-40 top-0 bg-black bg-opacity-75 h-full alumni-text ease-in-out duration-300 p-6 text-white text-xl  ">
-//     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia saepe atque quisquam iste tenetur? Cum voluptate, commodi consequatur maxime odio qui delectus id, hic, magnam eum dolores ipsum excepturi rerum!
-//             </div>
-//           </div>
-//         </div>
-//       </div><div class="swiper-slide w-max h-[460px] ">
-//         <div class="flex flex-col rounded shadow overflow-hidden">
-//           <div class="flex-shrink-0 relative alumni" onmouseover="show(this)" onmouseleave="hide(this)" onclick="show(this)">
-//             <img class="h-[50vh] w-full object-cover" src="https://images.squarespace-cdn.com/content/v1/5cdcc14ad86cc91b80cdbf65/1558486905146-N2NCK3RXK9ZJ1PLRMEDN/Raj+Headshot+2-2019.jpg?format=750w" alt="">
-// <div class="absolute bottom-0 p-6  z-50 alumni-content text-white">
-// <p class="text-3xl font-semibold">Raj soin</p>
-// <p >Class 1987</p>
-// </div>
-//             <div class="absolute z-40 top-0 bg-black bg-opacity-75 h-full alumni-text ease-in-out duration-300 p-6 text-white text-xl  ">
-//     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia saepe atque quisquam iste tenetur? Cum voluptate, commodi consequatur maxime odio qui delectus id, hic, magnam eum dolores ipsum excepturi rerum!
-//             </div>
-//           </div>
-//         </div>
-//       </div><div class="swiper-slide w-max h-[460px] ">
-//         <div class="flex flex-col rounded shadow overflow-hidden">
-//           <div class="flex-shrink-0 relative alumni" onmouseover="show(this)" onmouseleave="hide(this)" onclick="show(this)">
-//             <img class="h-[50vh] w-full object-cover" src="https://images.squarespace-cdn.com/content/v1/5cdcc14ad86cc91b80cdbf65/1558486905146-N2NCK3RXK9ZJ1PLRMEDN/Raj+Headshot+2-2019.jpg?format=750w" alt="">
-// <div class="absolute bottom-0 p-6  z-50 alumni-content text-white">
-// <p class="text-3xl font-semibold">Raj soin</p>
-// <p >Class 1987</p>
-// </div>
-//             <div class="absolute z-40 top-0 bg-black bg-opacity-75 h-full alumni-text ease-in-out duration-300 p-6 text-white text-xl  ">
-//     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia saepe atque quisquam iste tenetur? Cum voluptate, commodi consequatur maxime odio qui delectus id, hic, magnam eum dolores ipsum excepturi rerum!
-//             </div>
-//           </div>
-//         </div>
-//       </div><div class="swiper-slide w-max h-[460px] ">
-//         <div class="flex flex-col rounded shadow overflow-hidden">
-//           <div class="flex-shrink-0 relative alumni" onmouseover="show(this)" onmouseleave="hide(this)" onclick="show(this)">
-//             <img class="h-[50vh] w-full object-cover" src="https://images.squarespace-cdn.com/content/v1/5cdcc14ad86cc91b80cdbf65/1558486905146-N2NCK3RXK9ZJ1PLRMEDN/Raj+Headshot+2-2019.jpg?format=750w" alt="">
-// <div class="absolute bottom-0 p-6  z-50 alumni-content text-white">
-// <p class="text-3xl font-semibold">Raj soin</p>
-// <p >Class 1987</p>
-// </div>
-//             <div class="absolute z-40 top-0 bg-black bg-opacity-75 h-full alumni-text ease-in-out duration-300 p-6 text-white text-xl  ">
-//     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia saepe atque quisquam iste tenetur? Cum voluptate, commodi consequatur maxime odio qui delectus id, hic, magnam eum dolores ipsum excepturi rerum!
-//             </div>
-//           </div>
-//         </div>
-//       </div><div class="swiper-slide w-max h-[460px] ">
-//         <div class="flex flex-col rounded shadow overflow-hidden">
-//           <div class="flex-shrink-0 relative alumni" onmouseover="show(this)" onmouseleave="hide(this)" onclick="show(this)">
-//             <img class="h-[50vh] w-full object-cover" src="https://images.squarespace-cdn.com/content/v1/5cdcc14ad86cc91b80cdbf65/1558486905146-N2NCK3RXK9ZJ1PLRMEDN/Raj+Headshot+2-2019.jpg?format=750w" alt="">
-// <div class="absolute bottom-0 p-6  z-50 alumni-content text-white">
-// <p class="text-3xl font-semibold">Raj soin</p>
-// <p >Class 1987</p>
-// </div>
-//             <div class="absolute z-40 top-0 bg-black bg-opacity-75 h-full alumni-text ease-in-out duration-300 p-6 text-white text-xl  ">
-//     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia saepe atque quisquam iste tenetur? Cum voluptate, commodi consequatur maxime odio qui delectus id, hic, magnam eum dolores ipsum excepturi rerum!
-//             </div>
-//           </div>
-//         </div>
-//       </div>`;
+    document.getElementById('news_news').style.display = "none";
+    document.getElementById('donation_news').style.display = "block";
+    document.getElementById('donation_button').style.backgroundColor = "white";
+    document.getElementById('donation_button').style.color = "black";
+    document.getElementById('news_button').style.backgroundColor = "black";
+    document.getElementById('news_button').style.color = "white";
+  } else if (x === 'news') {
+    document.getElementById('news_news').style.display = "block";
+    document.getElementById('donation_news').style.display = "none";
+    document.getElementById('news_button').style.backgroundColor = "white";
+    document.getElementById('news_button').style.color = "black";
+    document.getElementById('donation_button').style.backgroundColor = "black";
+    document.getElementById('donation_button').style.color = "white";
+  }
+}
+
+function events(x) {
+  if (x === 'upcoming') {
+
+    document.getElementById('past_events').style.display = "none";
+    document.getElementById('upcoming_events').style.display = "block";
+    document.getElementById('upcoming_button').style.backgroundColor = "black";
+    document.getElementById('upcoming_button').style.color = "white";
+    document.getElementById('past_button').style.backgroundColor = "white";
+    document.getElementById('past_button').style.color = "black";
+  } else if (x === 'past') {
+    document.getElementById('past_events').style.display = "block";
+    document.getElementById('upcoming_events').style.display = "none";
+    document.getElementById('past_button').style.backgroundColor = "black";
+    document.getElementById('past_button').style.color = "white";
+    document.getElementById('upcoming_button').style.backgroundColor = "white";
+    document.getElementById('upcoming_button').style.color = "black";
+  }
+}
 
 
-//         document.getElementById("alumniCarousel").innerHTML += alum;
-//     }
-// );
 
 fetch("/data/donations.json").then(response => response.json())
     .then(data => {
@@ -102,24 +73,4 @@ fetch("/data/news.json").then(response => response.json())
         console.log(dat);
     });
 
-    fetch("/data/alumnicarousel.json").then(response => response.json()).then(data => {
-        console.log(data);
-        let alum = ``;
-        for(let item of data){
-            alum += ` <div class="swiper-slide  ">
-        <div class="flex flex-col rounded shadow overflow-hidden">
-          <div class="flex-shrink-0 relative alumni" onmouseover="show(this)" onmouseleave="hide(this)" onclick="show(this)">
-            <img class="h-[50vh] w-full object-cover" src="https://images.squarespace-cdn.com/content/v1/5cdcc14ad86cc91b80cdbf65/1558486905146-N2NCK3RXK9ZJ1PLRMEDN/Raj+Headshot+2-2019.jpg?format=750w" referrerpolicy="no-referrer" alt="">
-<div class="absolute bottom-0 p-6  z-50 alumni-content text-white">
-<p class="text-3xl font-semibold">Raj soin</p>
-<p >Class 1969</p>
-</div>
-            <div class="absolute z-40 top-0 bg-black bg-opacity-75 h-full alumni-text ease-in-out duration-300 p-6 text-white text-xl  ">
-Dr Rajesh K Soin, an alumnus of 1969 batch of Mechanical Engineering and now chairman and CEO of Soin LLC at Ohio in the US, donated the amount Rs 5 crore for the construction of a multipurpose hall at his alma mater Delhi Technological University
-            </div>
-          </div>
-        </div>
-      </div>`
-        }
-        // document.getElementById("alumnicarousel").innerHTML += alum;
-    });
+  
