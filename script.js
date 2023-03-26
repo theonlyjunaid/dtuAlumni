9c9487790200cc031b3af293bb02d3bcfcc9ecf6
shashwat-j <shashwatjain_ec21a18_23@dtu.ac.in>
2023-03-26 17:42:28 +0530

    events and animations

--- script.js
+++ script.js
@@ -73,4 +73,113 @@ fetch("/data/news.json").then(response => response.json())
         console.log(dat);
     });
 
+    // dates should ONLY be in these formats: 23-March-2023 or 23-Mar-2023 or 23 Mar 2023
+    fetch("./data/events.json").then(response => response.json())
+    .then(data => {
+        let dat = ``;
+        for(let item of data){
+            let pickedDatestr = item.date
+            var pickedDate = new Date(Date.parse(pickedDatestr.replace(/-/g, " ")));
+            var todaysDate = new Date();
+            var isUpcoming = pickedDate >= todaysDate;
+            
+            dat += isUpcoming? ` <li class="pb-3 sm:pb-4 pt-4">
+            <div class="flex flex-col md:flex-row items-center space-x-4 ">
+            <div class="flex-shrink-0">
+               <img class="h-24 w-60 object-cover rounded-md hover:cursor-pointer" src="${item.image}" alt="Neil image"
+               onClick="showModal('${item.image}')">
+            </div>
+            <div class="flex flex-row justify-between md:w-full">
+               <div>
+                 <p class="text-lg font-medium text-gray-900 truncate">
+                    ${item.title}
+                 </p>
+                 <div class="flex-row flex gap-2">
+                   <p class="text-sm text-gray-500 truncate">
+                      ${item.date}
+                   </p>
+                   <div class="inline-flex items-center md:hidden">
+                    <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full border border-green-600">Upcoming</span>
+                  </div>
+                 </div>
+               </div>
+              <div class="items-center hidden md:inline-flex">
+                <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full border border-green-600">Upcoming</span>
+              </div>
+            </div>
+            
+         </div>
+  </li>` : 
+  `
+  <li class="pb-3 sm:pb-4 pt-4">
+            <div class="flex flex-col md:flex-row items-center space-x-4 ">
+               <div class="flex-shrink-0">
+               <img class="h-24 w-60 object-cover rounded-md hover:cursor-pointer" src="${item.image}" alt="Neil image"
+               onClick="showModal('${item.image}')">
+            </div>
+            <div class="flex flex-row justify-between md:w-full">
+               <div>
+                 <p class="text-lg font-medium text-gray-900 truncate">
+                    ${item.title}
+                 </p>
+                 <div class="flex-row flex gap-2">
+                   <p class="text-sm text-gray-500 truncate">
+                      ${item.date}
+                   </p>
+                      <div class="inline-flex items-center md:hidden">
+                        <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full border border-red-400">past</span>
+                      </div>
+                    </div>
+                  </div>
+                 <div class="items-center hidden md:inline-flex">
+                  <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full border border-red-400">past</span>
+                </div>
+               </div>
+               
+            </div>
+          </li>
+         `;
+
+        }
+        document.getElementById("events_list").innerHTML += dat;
+        console.log(dat);
+    });
+    
+
   
+
+    // Events section image modal
+    var modal = document.getElementById("modal");
+    var modalImg = document.getElementById("modal-img");
+    function showModal(src) {
+        modal.classList.remove('hidden');
+        modalImg.src = src;
+    }
+    function closeModal() {
+        modal.classList.add('hidden');
+    }
+
+
+  //Numbers increase animation
+    const counterUp = window.counterUp.default
+
+    const callback = entries => {
+      entries.forEach( entry => {
+        const el = entry.target
+        if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+          counterUp( el, {
+            duration: 2000,
+            delay: 16,
+          } )
+          el.classList.add( 'is-visible' )
+        }
+      } )
+    }
+    
+    const IO = new IntersectionObserver( callback, { threshold: 1 } )
+    
+    //No. of iterations is equal to no. of stastistics that we are showing
+    for(i=0; i<4; i++){
+    const el = document.querySelectorAll( '.counter' )[i]
+    IO.observe( el )
+    }  
